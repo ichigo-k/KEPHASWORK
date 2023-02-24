5108453b457fc05b68f0848b86018c72f164812c
ichigo-k <125087781+ichigo-k@users.noreply.github.com>
2023-02-24 13:37:00 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+#Playing around with variales
+fname="Kephas"
+sname="Tetteh"
+age=16
+phonenumber="0249929828"
+int(phonenumber)
+
+#Concantenating a variable
+print( fname,sname,age, phonenumber)
+print ("your fullname is ;",fname ,sname)
+
+#Using python lists
+names_of_students=["Kephas","Micheal","Siaw"]
+print(names_of_students)
+second_student=names_of_students[1]
+print(second_student)
+names_of_students.append("john")
+print(names_of_students)
