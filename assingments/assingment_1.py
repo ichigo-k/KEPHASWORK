6ac522775e40749dfc3bfcc491f31555244d7dd7
ichigo_k <tettehsaintluke@gmail.com>
2023-04-04 11:41:48 +0000

    grade calculator

---
+++ assingments/assingment_1.py
@@ -0,0 +1,18 @@
+user_details=[]
+#NAME
+userinput = input("enter a name\n")
+user_details.append(userinput)
+#AGE
+userinput = input("enter age \n")
+user_details.append(userinput)
+#CLASS
+userinput = input("enter a class \n")
+user_details.append(userinput)
+#GENDER
+userinput = input("enter a gender \n")
+user_details.append(userinput)
+#TELEPHONE
+userinput = input("enter telephone number \n")
+user_details.append(userinput)
+
+print (f"your name is {user_details[0]}. your age is {user_details[1]}. your class is {user_details[2]}. your gender is {user_details[3]}. your telephone is {user_details[4]}")
---
+++ assingments/assingment_3.py
@@ -0,0 +1,15 @@
+
+grade=int(input("INPUT YOUR MARKS\n"))
+
+if grade >=85 :
+    print("A")
+elif grade >=75 :
+    print("B")
+elif grade >= 50:
+    print("C")
+elif grade >= 40:
+    print("D")
+elif grade >= 30:
+    print("E")
+else:
+    print("F")
