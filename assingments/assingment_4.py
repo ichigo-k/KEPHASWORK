194bc6f17b50dfaf86e1807e190f055f1bd73364
ichigo_k <tettehsaintluke@gmail.com>
2023-04-13 17:05:53 +0000

    average calculator

--- assingments/assingment_4.py
+++ assingments/assingment_4.py
@@ -1,12 +1,11 @@
 #SCRIPT FOR AN AVERAGE CALCULATOR
 values=[]
 total =0
-n= int(input("ENTER NUMBER OF VALUES"))
+n= int(input("ENTER TOTAL NUMBER OF VALUES\n"))
 for numbers in range (n):
-    num=int(input("ENTER A NUMBER"))
+    num=int(input("ENTER A NUMBER\n"))
     values.append(num)
     total =total + values[numbers]
-print(values)
 print(total)
 average=total/n
 print(average)
---
+++ assingments/kephas.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int data = 5;
+int value ;
+
+if(data == 70) {
+    digitalWrite(my name)
+}else{
+    digitalWrite(hello)
+}
